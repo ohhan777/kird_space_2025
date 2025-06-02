99f8029d1219ba17d04d28748aa8edc5176e2a15
Han Oh <ohhan777@gmail.com>
2025-06-02 23:22:39 +0900

    Update kari_objects_33cls_mini_evaluation_task1.py

--- mmrotate/DOTA_devkit/kari_objects_33cls_mini_evaluation_task1.py
+++ mmrotate/DOTA_devkit/kari_objects_33cls_mini_evaluation_task1.py
@@ -1,31 +1,14 @@
-# --------------------------------------------------------
-# dota_evaluation_task1
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Jian Ding, based on code from Bharath Hariharan
-# --------------------------------------------------------
-
-"""
-    To use the code, users should to config detpath, annopath and imagesetfile
-    detpath is the path for 15 result files, for the format, you can refer to "http://captain.whu.edu.cn/DOTAweb/tasks.html"
-    search for PATH_TO_BE_CONFIGURED to config the paths
-    Note, the evaluation is on the large scale images
-"""
 import xml.etree.ElementTree as ET
 import os
-#import cPickle
 import numpy as np
 import matplotlib.pyplot as plt
 import polyiou
 from functools import partial
+import glob
 
 def parse_gt(filename):
-    """
-
-    :param filename: ground truth file to parse
-    :return: all instances in a picture
-    """
     objects = []
-    with  open(filename, 'r') as f:
+    with open(filename, 'r') as f:
         while True:
             line = f.readline()
             if line:
@@ -34,11 +17,7 @@ def parse_gt(filename):
                 if (len(splitlines) < 9):
                     continue
                 object_struct['name'] = splitlines[8]
-
-                if (len(splitlines) == 9):
-                    object_struct['difficult'] = 0
-                elif (len(splitlines) == 10):
-                    object_struct['difficult'] = int(splitlines[9])
+                object_struct['difficult'] = 0
                 object_struct['bbox'] = [float(splitlines[0]),
                                          float(splitlines[1]),
                                          float(splitlines[2]),
@@ -51,14 +30,9 @@ def parse_gt(filename):
             else:
                 break
     return objects
+
 def voc_ap(rec, prec, use_07_metric=False):
-    """ ap = voc_ap(rec, prec, [use_07_metric])
-    Compute VOC AP given precision and recall.
-    If use_07_metric is true, uses the
-    VOC 07 11 point method (default:False).
-    """
     if use_07_metric:
-        # 11 point metric
         ap = 0.
         for t in np.arange(0., 1.1, 0.1):
             if np.sum(rec >= t) == 0:
@@ -67,137 +41,64 @@ def voc_ap(rec, prec, use_07_metric=False):
                 p = np.max(prec[rec >= t])
             ap = ap + p / 11.
     else:
-        # correct AP calculation
-        # first append sentinel values at the end
         mrec = np.concatenate(([0.], rec, [1.]))
         mpre = np.concatenate(([0.], prec, [0.]))
-
-        # compute the precision envelope
         for i in range(mpre.size - 1, 0, -1):
             mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
-
-        # to calculate area under PR curve, look for points
-        # where X axis (recall) changes value
         i = np.where(mrec[1:] != mrec[:-1])[0]
-
-        # and sum (\Delta recall) * prec
         ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
     return ap
 
-
-def voc_eval(detpath,
-             annopath,
-             imagesetfile,
-             classname,
-            # cachedir,
-             ovthresh=0.5,
-             use_07_metric=False):
-    """rec, prec, ap = voc_eval(detpath,
-                                annopath,
-                                imagesetfile,
-                                classname,
-                                [ovthresh],
-                                [use_07_metric])
-    Top level function that does the PASCAL VOC evaluation.
-    detpath: Path to detections
-        detpath.format(classname) should produce the detection results file.
-    annopath: Path to annotations
-        annopath.format(imagename) should be the xml annotations file.
-    imagesetfile: Text file containing the list of images, one image per line.
-    classname: Category name (duh)
-    cachedir: Directory for caching the annotations
-    [ovthresh]: Overlap threshold (default = 0.5)
-    [use_07_metric]: Whether to use VOC07's 11 point AP computation
-        (default False)
-    """
-    # assumes detections are in detpath.format(classname)
-    # assumes annotations are in annopath.format(imagename)
-    # assumes imagesetfile is a text file with each line an image name
-    # cachedir caches the annotations in a pickle file
-
-    # first load gt
-    #if not os.path.isdir(cachedir):
-     #   os.mkdir(cachedir)
-    #cachefile = os.path.join(cachedir, 'annots.pkl')
-    # read list of images
+def voc_eval(detpath, annopath, imagesetfile, classname, ovthresh=0.5, use_07_metric=False):
     with open(imagesetfile, 'r') as f:
         lines = f.readlines()
     imagenames = [x.strip() for x in lines]
-    #print('imagenames: ', imagenames)
-    #if not os.path.isfile(cachefile):
-        # load annots
+    
     recs = {}
     for i, imagename in enumerate(imagenames):
-        #print('parse_files name: ', annopath.format(imagename))
         recs[imagename] = parse_gt(annopath.format(imagename))
-        #if i % 100 == 0:
-         #   print ('Reading annotation for {:d}/{:d}'.format(
-          #      i + 1, len(imagenames)) )
-        # save
-        #print ('Saving cached annotations to {:s}'.format(cachefile))
-        #with open(cachefile, 'w') as f:
-         #   cPickle.dump(recs, f)
-    #else:
-        # load
-        #with open(cachefile, 'r') as f:
-         #   recs = cPickle.load(f)
-
-    # extract gt objects for this class
+    
     class_recs = {}
     npos = 0
     for imagename in imagenames:
         R = [obj for obj in recs[imagename] if obj['name'] == classname]
         bbox = np.array([x['bbox'] for x in R])
-        difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
+        difficult = np.array([bool(x['difficult']) for x in R])
         det = [False] * len(R)
-        npos = npos + sum(~difficult)
+        npos = npos + sum(difficult == False)
         class_recs[imagename] = {'bbox': bbox,
                                  'difficult': difficult,
                                  'det': det}
-
-    # read dets from Task1* files
+    
     detfile = detpath.format(classname)
     with open(detfile, 'r') as f:
         lines = f.readlines()
-
+    
     splitlines = [x.strip().split(' ') for x in lines]
     image_ids = [x[0] for x in splitlines]
     confidence = np.array([float(x[1]) for x in splitlines])
-
-    #print('check confidence: ', confidence)
-
     BB = np.array([[float(z) for z in x[2:]] for x in splitlines])
-
-    # sort by confidence
+    
     sorted_ind = np.argsort(-confidence)
     sorted_scores = np.sort(-confidence)
-
-    #print('check sorted_scores: ', sorted_scores)
-    #print('check sorted_ind: ', sorted_ind)
-
-    ## note the usage only in numpy not for list
     BB = BB[sorted_ind, :]
     image_ids = [image_ids[x] for x in sorted_ind]
-    #print('check imge_ids: ', image_ids)
-    #print('imge_ids len:', len(image_ids))
-    # go down dets and mark TPs and FPs
+    
     nd = len(image_ids)
     tp = np.zeros(nd)
     fp = np.zeros(nd)
+    
+    detection_results = {}
+    
     for d in range(nd):
         R = class_recs[image_ids[d]]
         bb = BB[d, :].astype(float)
         ovmax = -np.inf
         BBGT = R['bbox'].astype(float)
-
-        ## compute det bb with each BBGT
-
+        
         if BBGT.size > 0:
-            # compute overlaps
-            # intersection
-
-            # 1. calculate the overlaps between hbbs, if the iou between hbbs are 0, the iou between obbs are 0, too.
-            # pdb.set_trace()
+            # Compute overlaps
+            # Intersection
             BBGT_xmin =  np.min(BBGT[:, 0::2], axis=1)
             BBGT_ymin = np.min(BBGT[:, 1::2], axis=1)
             BBGT_xmax = np.max(BBGT[:, 0::2], axis=1)
@@ -215,7 +116,7 @@ def voc_eval(detpath,
             ih = np.maximum(iymax - iymin + 1., 0.)
             inters = iw * ih
 
-            # union
+            # Union
             uni = ((bb_xmax - bb_xmin + 1.) * (bb_ymax - bb_ymin + 1.) +
                    (BBGT_xmax - BBGT_xmin + 1.) *
                    (BBGT_ymax - BBGT_ymin + 1.) - inters)
@@ -225,21 +126,31 @@ def voc_eval(detpath,
             BBGT_keep_mask = overlaps > 0
             BBGT_keep = BBGT[BBGT_keep_mask, :]
             BBGT_keep_index = np.where(overlaps > 0)[0]
-            # pdb.set_trace()
+
             def calcoverlaps(BBGT_keep, bb):
                 overlaps = []
                 for index, GT in enumerate(BBGT_keep):
-
                     overlap = polyiou.iou_poly(polyiou.VectorDouble(BBGT_keep[index]), polyiou.VectorDouble(bb))
                     overlaps.append(overlap)
                 return overlaps
+
             if len(BBGT_keep) > 0:
                 overlaps = calcoverlaps(BBGT_keep, bb)
-
                 ovmax = np.max(overlaps)
                 jmax = np.argmax(overlaps)
-                # pdb.set_trace()
                 jmax = BBGT_keep_index[jmax]
+
+        is_correct = ovmax > ovthresh and not R['difficult'][jmax] if ovmax > -np.inf else False
+
+        if image_ids[d] not in detection_results:
+            detection_results[image_ids[d]] = []
+        
+        detection_results[image_ids[d]].append({
+            'bbox': bb.tolist(),
+            'class_name': classname,
+            'score': confidence[sorted_ind[d]],
+            'is_correct': is_correct
+        })
 
         if ovmax > ovthresh:
             if not R['difficult'][jmax]:
@@ -250,68 +161,74 @@ def voc_eval(detpath,
                     fp[d] = 1.
         else:
             fp[d] = 1.
-
-    # compute precision recall
-
-    print('check fp:', fp)
-    print('check tp', tp)
-
-
-    print('npos num:', npos)
+    
     fp = np.cumsum(fp)
     tp = np.cumsum(tp)
-
     rec = tp / float(npos)
-    # avoid divide by zero in case the first detection matches a difficult
-    # ground truth
     prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
     ap = voc_ap(rec, prec, use_07_metric)
-
-    return rec, prec, ap
+    
+    return rec, prec, ap, detection_results
 
 def main():
 
-    # ##TODO: wrap the code in the main
-    # detpath = r'/home/dingjian/Documents/Research/experiments/light_head_faster_rotbox_best_point/Task1_results_0.1_nms_epoch18/results/Task1_{:s}.txt'
-    # annopath = r'/home/dingjian/code/DOTA/DOTA/media/OrientlabelTxt-utf-8/{:s}.txt'# change the directory to the path of val/labelTxt, if you want to do evaluation on the valset
-    # imagesetfile = r'/home/dingjian/code/DOTA/DOTA/media/testset.txt'
-    # classnames = ['plane', 'baseball-diamond', 'bridge', 'ground-track-field', 'small-vehicle', 'large-vehicle', 'ship', 'tennis-court',
-    #             'basketball-court', 'storage-tank',  'soccer-ball-field', 'roundabout', 'harbor', 'swimming-pool', 'helicopter']
+    detpath = r'work_dirs/Task1_results/Task1_{:s}.txt'
+    annopath = r'data/kari_objects_33cls_mini/val/dota/{:s}.txt'
+    img_path = r'data/kari_objects_33cls_mini/val/images'
+    img_list_file = r'data/kari_objects_33cls_mini_val_image_list.txt'
+    import glob
+    img_files = glob.glob(os.path.join(img_path, '*.png'))
+    img_files = [os.path.basename(img_file).split('.')[0] for img_file in img_files]
 
-    detpath = r'PATH_TO_BE_CONFIGURED/Task1_{:s}.txt'
-    annopath = r'PATH_TO_BE_CONFIGURED/{:s}.txt' # change the directory to the path of val/labelTxt, if you want to do evaluation on the valset
-    imagesetfile = r'PATH_TO_BE_CONFIGURED/valset.txt'
+    with open(img_list_file, 'w') as f:
+        for img_file in img_files:
+            f.write(img_file + '\n')
 
-    # For DOTA-v1.5
-    # classnames = ['plane', 'baseball-diamond', 'bridge', 'ground-track-field', 'small-vehicle', 'large-vehicle', 'ship', 'tennis-court',
-    #             'basketball-court', 'storage-tank',  'soccer-ball-field', 'roundabout', 'harbor', 'swimming-pool', 'helicopter', 'container-crane']
-    # For DOTA-v1.0
-    classnames = ['plane', 'baseball-diamond', 'bridge', 'ground-track-field', 'small-vehicle', 'large-vehicle', 'ship', 'tennis-court',
-                'basketball-court', 'storage-tank',  'soccer-ball-field', 'roundabout', 'harbor', 'swimming-pool', 'helicopter']
+    classnames = ['motorboat', 'sailboat', 'tugboat', 'barge', 'fishing_boat', 'ferry', 'container_ship', 
+               'oil_tanker', 'drill_ship', 'warship', 'fighter_aircraft', 'large_military_aircraft', 'small_civilian_aircraft', 
+               'large_civilian_aircraft', 'helicopter', 'small_vehicle', 'truck', 'bus', 'train', 'container', 'container_group', 
+               'crane', 'bridge', 'dam', 'storage_tank', 'sports_field', 'stadium', 'swimming_pool', 'roundabout', 'helipad', 
+               'wind_turbine', 'aquaculture_farm', 'marine_research_station']
+
+    all_detection_results = {}
     classaps = []
     map = 0
     for classname in classnames:
         print('classname:', classname)
-        rec, prec, ap = voc_eval(detpath,
+        rec, prec, ap, class_results = voc_eval(detpath,
              annopath,
-             imagesetfile,
+             img_list_file,
              classname,
              ovthresh=0.5,
              use_07_metric=True)
-        map = map + ap
-        #print('rec: ', rec, 'prec: ', prec, 'ap: ', ap)
+        
+        for image_id, results in class_results.items():
+            if image_id not in all_detection_results:
+                all_detection_results[image_id] = []
+            all_detection_results[image_id].extend(results)
+        
+        map += ap
         print('ap: ', ap)
         classaps.append(ap)
 
-        # umcomment to show p-r curve of each category
-        # plt.figure(figsize=(8,4))
-        # plt.xlabel('recall')
-        # plt.ylabel('precision')
-        # plt.plot(rec, prec)
-       # plt.show()
-    map = map/len(classnames)
+    map = map / len(classnames)
     print('map:', map)
-    classaps = 100*np.array(classaps)
+    classaps = 100 * np.array(classaps)
     print('classaps: ', classaps)
+
+    # 결과를 파일에 저장
+    output_dir = 'detection_results_kari_objects_33cls'
+    os.makedirs(output_dir, exist_ok=True)
+    
+    for image_id, results in all_detection_results.items():
+        output_file = os.path.join(output_dir, f"{image_id}_det.txt")
+        with open(output_file, 'w') as f:
+            for result in results:
+                bbox = result['bbox']
+                bbox_str = ' '.join([f"{coord:.6f}" for coord in bbox])
+                f.write(f"{bbox_str} {result['class_name']} {result['score']:.6f} {int(result['is_correct'])}\n")
+    
+    print(f"Detection results saved to {output_dir} directory")
+
 if __name__ == '__main__':
     main()
